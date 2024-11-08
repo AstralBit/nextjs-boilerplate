89b1eb22322867f54c239b3a89fb4a8689b9d6dd
karl-s-fe <karl-s-fe@gate.me>
2024-11-08 14:55:03 +0800

    feat: add pthots

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+"use client";
 
-import { Transition } from "@/app/components/motion-transition";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -14,28 +14,21 @@ export default function Page() {
         height={160}
         priority
       />
+      <h1 className="mb-8 text-2xl font-medium tracking-tight">
+        你好，欢迎来到我的博客！
+      </h1>
 
-      <Transition>
-        <h1 className="mb-8 text-2xl font-medium tracking-tight">
-          你好，欢迎来到我的博客！
-        </h1>
-      </Transition>
-
-      <Transition>
-        <div className="prose prose-neutral dark:prose-invert">
-          <p>始终相信，代码可以理性，但生活需要感性。</p>
-          <p>
-            在这里，我会分享一些技术心得，也会聊聊生活中那些编程之外的时刻。
-          </p>
-          <p>
-            骑上车，或者翻几页喜欢的书，看一部经典老电影。人生苦短，代码再多，也要留点时间给诗和远方。
-          </p>
-          <p>
-            希望在这片小小的博客空间里，能找到同样热爱自由、享受浪漫的你们。
-            人生旅途漫长，代码之外，愿我们都能拥有属于自己的诗和远方。
-          </p>
-        </div>
-      </Transition>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>始终相信，代码可以理性，但生活需要感性。</p>
+        <p>在这里，我会分享一些技术心得，也会聊聊生活中那些编程之外的时刻。</p>
+        <p>
+          骑上车，或者翻几页喜欢的书，看一部经典老电影。人生苦短，代码再多，也要留点时间给诗和远方。
+        </p>
+        <p>
+          希望在这片小小的博客空间里，能找到同样热爱自由、享受浪漫的你们。
+          人生旅途漫长，代码之外，愿我们都能拥有属于自己的诗和远方。
+        </p>
+      </div>
     </section>
   );
 }
