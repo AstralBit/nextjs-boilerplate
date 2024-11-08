d023b6829c27b3112f69858c9d0e63c08acfc632
karl-s-fe <karl-s-fe@gate.me>
2024-11-08 14:24:33 +0800

    feat: add pthots

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -19,41 +19,39 @@ export default function Photos() {
             alt: "Roman columns",
             href: "https://unsplash.com/photos/people-walking-near-building-during-daytime-dFLBDQQeffU?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
           },
-          {
-            src: "/photos/photo2.jpg",
-            alt: "Big Ben",
-            href: "https://unsplash.com/photos/big-ben-london-MdJq0zFUwrw?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
-          },
-          {
-            src: "/photos/photo3.jpg",
-            alt: "Sacré-Cœur Basilica",
-            href: "https://unsplash.com/photos/a-view-of-the-inside-of-a-building-through-a-circular-window-Tp-3hrx88J4",
-          },
-          {
-            src: "/photos/photo4.jpg",
-            alt: "Eiffel Tower",
-            href: "https://unsplash.com/photos/the-eiffel-tower-towering-over-the-city-of-paris-OgPuPvPsHLM?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
-          },
-          {
-            src: "/photos/photo5.jpg",
-            alt: "Taj Mahal",
-            href: "https://unsplash.com/photos/taj-mahal-india-IPlPkWPJ2fo",
-          },
-          {
-            src: "/photos/photo6.jpg",
-            alt: "Colosseum",
-            href: "https://unsplash.com/photos/brown-concrete-building-under-blue-sky-during-daytime-3cyBR1rIJmA?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
-          },
         ]}
       /> */}
 
       <ImageGrid
+        columns={3}
+        images={[
+          { src: "/photos/p1.jpeg", alt: "error" },
+          { src: "/photos/p2.jpeg", alt: "error" },
+          { src: "/photos/p3.jpeg", alt: "error" },
+        ]}
+      />
+
+      <ImageGrid
         columns={2}
         images={[
-          { src: "/photos/photo1.jpg", alt: "Roman columns" },
-          { src: "/photos/photo2.jpg", alt: "Big Ben" },
-          { src: "/photos/photo3.jpg", alt: "Sacré-Cœur Basilica" },
-          { src: "/photos/photo4.jpg", alt: "Eiffel Tower" },
+          { src: "/photos/p4.jpeg", alt: "error" },
+          { src: "/photos/p5.jpeg", alt: "error" },
+          { src: "/photos/p6.jpeg", alt: "error" },
+          { src: "/photos/p7.jpeg", alt: "error" },
+        ]}
+      />
+
+      <ImageGrid
+        columns={3}
+        images={[
+          { src: "/photos/p8.jpeg", alt: "error" },
+          { src: "/photos/p9.jpeg", alt: "error" },
+          { src: "/photos/p10.jpeg", alt: "error" },
+          { src: "/photos/p11.jpeg", alt: "error" },
+          { src: "/photos/p12.jpeg", alt: "error" },
+          { src: "/photos/p13.jpeg", alt: "error" },
+          { src: "/photos/p14.jpeg", alt: "error" },
+          { src: "/photos/p15.jpeg", alt: "error" },
         ]}
       />
     </section>
