98eccc86cbeaa34a3b180ae4a38bb0386e984145
karl-s-fe <karl-s-fe@gate.me>
2025-06-24 09:28:39 +0800

    feat: bg

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -8,19 +8,19 @@ import { random } from 'maath';
 
 function ParticleField() {
   const ref = useRef<THREE.Points>(null);
-  const count = 1000; // 减少粒子数量
+  const count = 300; // 减少粒子数量
   
   // 生成随机粒子位置
   const positions = useMemo(() => {
     const positions = new Float32Array(count * 3);
-    random.inSphere(positions, { radius: 5 });
+    random.inSphere(positions, { radius: 10 });
     return positions;
   }, []);
 
   // 简化动画效果
   useFrame((state, delta) => {
     if (ref.current) {
-      ref.current.rotation.y -= delta / 20; // 只保留一个方向的旋转
+      ref.current.rotation.y -= delta / 90; // 只保留一个方向的旋转
     }
   });
 
